e214efac8c1c98ba49d1282d748c9a487eec7853
Alexander Leino <Alexander.Leino@yahoo.com>
2024-01-10 23:07:55 -0500

    add carousel to ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,42 @@
 import Image from 'next/image'
+import chess from '../public/chessclub.jpg'
+import chessWinner from '../public/chess-winner.jpg'
+import chessAllAges from '../public/chess-all-ages.jpg'
+import chessBoard from '../public/chessboard.jpg'
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='text-amber-900'>Welcome To Premier Kalamazoo Chess Club</div>
-    </main>
+<main className="flex min-h-screen flex-col items-center justify-between p-24">
+<Carousel
+opts={{
+  align: "center",
+  loop: true,
+  active: true,
+  
+  }}>
+<CarouselPrevious/>
+<CarouselContent>
+<CarouselItem className='flex justify-center'>
+<Image src={chess} height={850} alt='something spicy' />
+</CarouselItem>
+<CarouselItem className='flex justify-center'>
+<Image src={chessWinner} height={850} alt='something spicy' />
+</CarouselItem>
+<CarouselItem className='flex justify-center'>
+<Image src={chessAllAges} height={850} alt='something spicy' />
+</CarouselItem>
+</CarouselContent>
+<CarouselNext/>
+</Carousel>
+
+</main>
   )
 }
